b6c08ff79fed0263791ec9aee156720b334a704d
Przemek B <pprzemek89@gmail.com>
2024-02-22 12:11:04 +0100

    comit z lekcji od pana filipa

--- src/wypozyczenie/Egzemplarz.java
+++ src/wypozyczenie/Egzemplarz.java
@@ -2,8 +2,38 @@ package wypozyczenie;
 
 import ksiazka.Ksiazka;
 
-public class Egzemplarz {
+import java.util.Objects;
 
+public class Egzemplarz {
+    private Ksiazka ksiazka;
+
+    public enum Stan{
+        WYPOZYCZONY,WOLNY
+    }
+    private Stan stan;
+
+    public Egzemplarz(Ksiazka ksiazka) {
+        this.ksiazka = ksiazka;
+        stan = Stan.WOLNY;
+    }
+
+    public Stan getStan() {
+        return stan;
+    }
+
+    public void setStan(Stan stan) {
+        this.stan = stan;
+    }
+
+    public Ksiazka getKsiazka() {
+        return ksiazka;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Egzemplarz that)) return false;
+        return Objects.equals(ksiazka, that.ksiazka) && stan == that.stan;
+    }
 
 }
-
--- src/wypozyczenie/Wypozyczenie.java
+++ src/wypozyczenie/Wypozyczenie.java
@@ -1,4 +1,37 @@
 package wypozyczenie;
 
+import java.time.Duration;
+import java.time.LocalDate;
+
 public class Wypozyczenie {
+
+    private static final int DLUGOSC_WYPOZYCZENIA = 30;
+    private static final double KARA = 0.1;
+    private Egzemplarz egzemplarz;
+    private LocalDate dataWypozyczenia;
+    private LocalDate dataOddania;
+    private LocalDate przewidywanaDataOddanial;
+
+    public Wypozyczenie(Egzemplarz egzemplarz) {
+        this.egzemplarz = egzemplarz;
+        egzemplarz.setStan(Egzemplarz.Stan.WYPOZYCZONY);
+        dataWypozyczenia = LocalDate.now();
+        przewidywanaDataOddanial = dataWypozyczenia.plusDays(DLUGOSC_WYPOZYCZENIA);
+        dataOddania = null;
+
+    }
+    public double oddaj(){
+        egzemplarz.setStan(Egzemplarz.Stan.WOLNY);
+        dataOddania=LocalDate.now();
+        if(przewidywanaDataOddanial.isBefore(dataOddania)){
+            long liczbaDni = Duration.between(dataOddania,przewidywanaDataOddanial).toDays();
+            return liczbaDni*KARA;
+        }
+        return 0;
+    }
+
+    public LocalDate prelonguj(){
+        przewidywanaDataOddanial = przewidywanaDataOddanial.plusDays(DLUGOSC_WYPOZYCZENIA);
+        return przewidywanaDataOddanial;
+    }
 }
